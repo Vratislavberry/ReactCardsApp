14a97e9c0e57c2b266b94f5dd1e07be29b8491a3
Vratislav Beran <beran.vratislav23@gmail.com>
2024-12-18 13:48:05 +0100

    SplitCard and SplitCardSegment are functional

--- src/components/SplitCard.jsx
+++ src/components/SplitCard.jsx
@@ -1,15 +1,59 @@
 import React from "react";
 import SplitCardSegment from "./SplitCardSegment";
 
-// divides given text into buttons
 function SplitCard(props) {
+  // Init of textSegments hook
+  const [textSegments, setTextSegments] = React.useState(
+    props.text.split(" ").map((textPart, index) => {
+      return { id: index, text: textPart, checked: false };
+    })
+  );
+
+  console.log(textSegments);
+
+  // Handle functions
+  function handleCardSegmentClick(id) {
+   const tempObject = textSegments.find((segment) => {return segment.id === id})
+   tempObject.checked = !tempObject.checked;
+    setTextSegments((preValue) => {
+      return [
+        ...preValue.filter((segment) => {
+          return segment.id !== id;
+        }),
+        tempObject,
+      ];
+    });
+  }
+
+  // helper functions
+  function getSpecifiedSegments(textSegments, myFilter) {
+    return textSegments
+      .filter(myFilter)
+      .map((textSegment) => (
+        <SplitCardSegment
+          onClick={handleCardSegmentClick}
+          key={textSegment.id}
+          id={textSegment.id}
+          text={textSegment.text}
+          checked={textSegment.checked}
+        />
+      ));
+  }
+
   return (
     <div className="splitCard">
-      <p>{props.title}</p>
+      <h3>{props.title}</h3>
       <hr />
-      {props.text.split(" ").map((textSegment, index) => (
-        <SplitCardSegment key={index} id={index} text={textSegment} />
-      ))}
+      <p>Odpověď: </p>
+      {getSpecifiedSegments(textSegments, (textSegment) => {
+        return textSegment.checked === true;
+      })}
+
+      <hr />
+      <p>Výběr</p>
+      {getSpecifiedSegments(textSegments, (textSegment) => {
+        return textSegment.checked === false;
+      })}
     </div>
   );
 }
--- src/components/SplitCardSegment.jsx
+++ src/components/SplitCardSegment.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SplitCardSegment(props) {
-    return <button className="splitCardSegment">{props.text}</button>
+    return <button onClick={() => {props.onClick(props.id)}} className="splitCardSegment">{props.text}</button>
 }
 
 export default SplitCardSegment;
