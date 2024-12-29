72c8d09c889b90b2be552cc65c2c88c9c6498832
Vratislav Beran <beran.vratislav23@gmail.com>
2024-12-29 11:23:44 +0100

    splitCards are now stored in persistent browser storage

--- src/pages/CardManager.jsx
+++ src/pages/CardManager.jsx
@@ -3,12 +3,20 @@ import FormAddSplitCard from "../components/FormAddSplitCard";
 import SplitCard from "../components/SplitCard";
 
 function CardManager() {
-  const [splitCards, setSplitCards] = React.useState([
-    {
-      title: "Tutorial",
-      text: "Hello, how are you?",
-    },
-  ]);
+  const [splitCards, setSplitCards] = React.useState(() => {
+    const data = window.localStorage.getItem("MY_SPLIT_CARDS");
+    return data ? JSON.parse(data) : [
+      {
+        title: "Tutorial",
+        text: "Hello, how are you?",
+      }
+    ];
+  });
+
+  React.useEffect(() => {
+    window.localStorage.setItem("MY_SPLIT_CARDS", JSON.stringify(splitCards));
+    console.log(splitCards);
+  }, [splitCards]);
 
   function addSplitCard(newCard) {
     if (newCard.title == "" || newCard.text == "") {
